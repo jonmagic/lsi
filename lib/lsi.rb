82b4017cb88e01b089379d49ba08766aad86227e
Jonathan Hoyt <hoyt@github.com>
2016-08-11 21:35:20 -0700

    Start moving things into Lsi class

--- lib/lsi.rb
+++ lib/lsi.rb
@@ -1,4 +1,57 @@
 require "lsi/version"
 
 class Lsi
+  def self.run(command:, path:, list_command:)
+    apply = if command
+      lambda do |path|
+        stdout, stderr, status = Open3.capture3("#{command} #{path}")
+
+        if status.success?
+          puts stdout
+        else
+          puts stderr
+        end
+      end
+    else
+      lambda do |path|
+        filename = File.basename(path)
+        stat = File.stat(path)
+
+        if stat.directory?
+          puts "#{filename} is a directory with permissions #{sprintf("%o", stat.mode)}"
+        else
+          puts "#{filename} is a #{stat.size} byte file with permissions #{sprintf("%o", stat.mode)}"
+        end
+      end
+    end
+
+    list = if list_command
+      stdout, stderr, status = Open3.capture3(list_command)
+
+      if status.success?
+        stdout.split("\n")
+      end
+    end
+
+    (list || Dir["#{path || Dir.pwd}/*"]).each do |item|
+      filename = File.basename(item)
+
+      if command
+        print "Run `#{command} #{filename}`? [y,n,q] (y): "
+      else
+        print "Get info for `#{filename}`? [y,n,q] (y): "
+      end
+
+      case STDIN.gets.chomp
+      when "y", ""
+        apply.call(item)
+      when "n"
+        next
+      when "q"
+        exit
+      else
+        exit!
+      end
+    end
+  end
 end
